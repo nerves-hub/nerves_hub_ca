4ed0220e1620b205f4f2d66b9be4b65c20c229a9
Justin Schneck <jschneck@mac.com>
2018-10-30 16:10:06 -0400

    Add seed file

---
+++ priv/repo/seeds.exs
@@ -0,0 +1,15 @@
+# Script for populating the database. You can run it as:
+#
+#     mix run priv/repo/seeds.exs
+#
+# Inside the script, you can read and write to any of your
+# repositories directly:
+#
+#     NervesHubCA.Repo.insert!(%NervesHubCA.SomeSchema{})
+#
+# We recommend using the bang functions (`insert!`, `update!`
+# and so on) as they will fail if something goes wrong.
+
+# The seeds are run on every deploy. Therefore, it is important
+# that first check to see if the data you are trying to insert
+# has been run yet.
