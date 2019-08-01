9bbf4d1e79686fd7fbfe401e87b568d32d101d45
Justin Schneck <jschneck@mac.com>
2019-08-01 12:02:54 -0400

    Allow the hostname to be set from the runtime

--- config/prod.exs
+++ config/prod.exs
@@ -1,3 +1,12 @@
 use Mix.Config
 
 config :nerves_hub_ca, NervesHubCA.Repo, adapter: Ecto.Adapters.Postgres
+
+working_dir = "/etc/ssl"
+
+config :nerves_hub_ca, :api,
+  otp_app: :nerves_hub_ca,
+  port: 8443,
+  cacertfile: Path.join(working_dir, "ca.pem"),
+  certfile: Path.join(working_dir, "${HOST}.pem"),
+  keyfile: Path.join(working_dir, "${HOST}-key.pem")
