53c035d7b62108667313409bc1d2ca700947f695
Justin Schneck <jschneck@mac.com>
2019-08-01 12:05:18 -0400

    Add staging deploy phase and require prod approval

--- rel/scripts/ecs-network-configuration.sh
+++ rel/scripts/ecs-network-configuration.sh
@@ -2,10 +2,13 @@
 
 set -e
 
-VPCS=$(aws ec2 describe-vpcs --output json | jq ".Vpcs")
-VPC=$(echo $VPCS | jq '.[] | . as $vpc | .Tags[] | select(.Key == "Name" and .Value == "nerves-hub") | $vpc.VpcId')
+VPC=$1
+SG=$2
 
-SUBNETS=$(aws ec2 describe-subnets --filters=Name=vpc-id,Values=$VPC,Name=tag:Name,Values=nerves-hub/Private --output json | jq -c '[.Subnets[] | .SubnetId]')
-SECURITY_GROUPS=$(aws ec2 describe-security-groups --filters=Name=tag:Name,Values=nerves-hub-www-sg | jq -c '[.SecurityGroups[] | .GroupId ]')
+VPCS=$(aws ec2 describe-vpcs --output json | jq ".Vpcs")
+VPC=$(echo $VPCS | jq --arg VPC $VPC '.[] | . as $vpc | .Tags[] | select(.Key == "Name" and .Value == $VPC) | $vpc.VpcId')
+
+SUBNETS=$(aws ec2 describe-subnets --filters=Name=vpc-id,Values=$VPC,Name=tag:Name,Values=*private* --output json | jq -c '[.Subnets[] | .SubnetId]')
+SECURITY_GROUPS=$(aws ec2 describe-security-groups --filters=Name=tag:Name,Values=$SG | jq -c '[.SecurityGroups[] | .GroupId ]')
 
 echo "awsvpcConfiguration={subnets=$SUBNETS,securityGroups=$SECURITY_GROUPS,assignPublicIp=DISABLED}"
