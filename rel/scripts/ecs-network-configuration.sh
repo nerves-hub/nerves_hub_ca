ca8f1175cc1102f54a7de53885354127a7f3a695
Justin Schneck <jschneck@mac.com>
2019-08-01 17:24:04 -0400

    Update network config with AND filtering

--- rel/scripts/ecs-network-configuration.sh
+++ rel/scripts/ecs-network-configuration.sh
@@ -6,9 +6,8 @@ VPC=$1
 SG=$2
 
 VPCS=$(aws ec2 describe-vpcs --output json | jq ".Vpcs")
-VPC=$(echo $VPCS | jq --arg VPC $VPC '.[] | . as $vpc | .Tags[] | select(.Key == "Name" and .Value == $VPC) | $vpc.VpcId')
-
-SUBNETS=$(aws ec2 describe-subnets --filters=Name=vpc-id,Values=$VPC,Name=tag:Name,Values=*private* --output json | jq -c '[.Subnets[] | .SubnetId]')
+VPC=$(echo $VPCS | jq --arg vpc $VPC '.[] | select((.Tags[]|select(.Key=="Name")|.Value) == $vpc) | .VpcId')
+SUBNETS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VPC" --output json | jq -c '[.Subnets[] | select((.Tags[]|select(.Key=="Name")|.Value) | contains("private")) | .SubnetId]')
 SECURITY_GROUPS=$(aws ec2 describe-security-groups --filters=Name=tag:Name,Values=$SG | jq -c '[.SecurityGroups[] | .GroupId ]')
 
 echo "awsvpcConfiguration={subnets=$SUBNETS,securityGroups=$SECURITY_GROUPS,assignPublicIp=DISABLED}"
